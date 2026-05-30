6c12e327bda604fc08f2160e30500031d7e7feaf
study <veritasdevar@gmail.com>
2026-05-30 11:49:59 +0000

    modified:   config/app.php 	modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -53,11 +53,13 @@
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
 
-                        <x-dropdown-link :href="route('logout')"
-                            onclick="event.preventDefault(); this.closest('form').submit();">
-                            {{ __('Log Out') }}
-                        </x-dropdown-link>
-                    </form>
+                        <x-dropdown-link
+        :href="route('logout')"
+        dusk="logout"
+        onclick="event.preventDefault(); this.closest('form').submit();">
+        {{ __('Log Out') }}
+    </x-dropdown-link>
+</form>
                 </x-slot>
             </x-dropdown>
         </div>
