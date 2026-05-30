08e32752f4e0d48fd999234e6978e6612a77bda6
study <veritasdevar@gmail.com>
2026-05-30 11:17:10 +0000

    modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,18 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    @auth
+        @include('layouts.navigation')
+    @endauth
+
     <div class="container mx-auto py-4">
         <h1>Привет от Хекслета!</h1>
     </div>
-</x-app-layout>
+</body>
+</html>
