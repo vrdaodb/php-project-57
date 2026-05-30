36cfb3a85e4d539d78c1f3f1f19b861f37f56092
study <veritasdevar@gmail.com>
2026-05-30 11:00:55 +0000

    modified:   lang/ru/validation.php 	modified:   resources/views/welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name') }}</title>
-</head>
-<body>
-    <h1>Привет от Хекслета!</h1>
-
-    @auth
-        <a href="{{ route('dashboard') }}">Dashboard</a>
-    @else
-        <a href="{{ route('login') }}">Вход</a>
-
-        @if (Route::has('register'))
-            <a href="{{ route('register') }}">Регистрация</a>
-        @endif
-    @endauth
-</body>
-</html>
+<x-app-layout>
+    <div class="container mx-auto py-4">
+        <h1>Привет от Хекслета!</h1>
+    </div>
+</x-app-layout>
