3389ed9a18021bcffcc6206e8a90d1261759f2cb
study <veritasdevar@gmail.com>
2026-05-30 12:15:34 +0000

    modified:   config/app.php

--- config/app.php
+++ config/app.php
@@ -78,9 +78,8 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'ru'),
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'ru'),
-
+    'locale' => 'ru',
+    'fallback_locale' => 'ru',
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
     /*
