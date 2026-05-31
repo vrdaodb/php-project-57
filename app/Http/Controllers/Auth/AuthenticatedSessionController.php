7eea974ed0834990df999840f74e499f5c934185
study <veritasdevar@gmail.com>
2026-05-31 17:30:34 +0000

    Route fix

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -23,13 +23,13 @@ class AuthenticatedSessionController extends Controller
      * Handle an incoming authentication request.
      */
     public function store(LoginRequest $request): RedirectResponse
-    {
-        $request->authenticate();
+{
+    $request->authenticate();
 
-        $request->session()->regenerate();
+    $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
-    }
+    return redirect()->intended('/');
+}
 
     /**
      * Destroy an authenticated session.
