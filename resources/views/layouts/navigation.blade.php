3c2541799cb0aec5ee2280e355462af95c5d9b84
study <veritasdevar@gmail.com>
2026-06-01 14:11:54 +0000

    modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -9,7 +9,7 @@
                     </a>
                 </div>
             <!-- Navigation Links -->
-            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+            <div class="flex space-x-8 sm:-my-px sm:ms-10">
                 <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Главная') }}
                 </x-nav-link>
@@ -29,7 +29,7 @@
         </div>
 
         <!-- Settings Dropdown -->
-        <div class="hidden sm:flex sm:items-center sm:ms-6">
+        <div class="flex sm:items-center sm:ms-6">
             @auth
             <x-dropdown align="right" width="48">
                 <x-slot name="trigger">
