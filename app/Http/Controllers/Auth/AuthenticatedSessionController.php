deb2ee5eeb7fce71c0710b535341e584a02bf17c
study <veritasdevar@gmail.com>
2026-06-01 13:55:53 +0000

    authcontroller

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
     $request->session()->regenerate();
 
-    return redirect()->intended('/dashboard');
+    return redirect()->intended('/');
 }
 
     /**
